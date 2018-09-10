a47e86dc95efd08a9d3831da9ae356dc1a8ac9ae
MED <marianodeleu@yahoo.com.ar>
2018-09-10 14:28:21 -0300

    termino de migrar spi

--- src/spi.c
+++ src/spi.c
@@ -1,122 +1,25 @@
+//---------------------------------------------
+// ##
+// ## @Author: Med
+// ## @Editor: Emacs - ggtags
+// ## @TAGS:   Global
+// ## @CPU:    STM32F030
+// ##
+// #### SPI.C #################################
+//---------------------------------------------
+
+/* Includes ------------------------------------------------------------------*/
 #include "spi.h"
 #include "stm32f0xx.h"
-
-//desde que saque main.h
 #include "stm32f0xx_gpio.h"
-#include "stm32f0xx_spi.h"
-#include "stm32f0xx_rcc.h"
-
-#define SPIx                             SPI1
-//fin desde que saque main.h
 
 
-//-------------------------- EXTERNAL VARIABLES ------------------------
-extern volatile unsigned char TxBuffer_SPI [];
-extern volatile unsigned char RxBuffer_SPI [];
-extern volatile unsigned char *pspi_tx;
-extern volatile unsigned char *pspi_rx;
-extern volatile unsigned char spi_bytes_left;
-//extern SPI_InitTypeDef  SPI_InitStructure;
+/* Externals ------------------------------------------------------------------*/
 
-//-------------------------- GLOBAL VARIABLES --------------------------
-//SPI_InitTypeDef  SPI_InitStructure;
-
-//--------------------------- FUNCTIONS --------------------------------
-
-/**
-  * @brief  Configures the SPI Peripheral.
-  * @param  None
-  * @retval None
-  */
-// void SPI_Config(void)
-// {
-//   GPIO_InitTypeDef GPIO_InitStructure;
-//   //NVIC_InitTypeDef NVIC_InitStructure;
-//   SPI_InitTypeDef  SPI_InitStructure;
-
-//   /* Enable the SPI periph */
-//   RCC_APB2PeriphClockCmd(SPIx_CLK, ENABLE);
-
-//   /* Enable SCK, MOSI, MISO and NSS GPIO clocks */
-//   RCC_AHBPeriphClockCmd(SPIx_SCK_GPIO_CLK | SPIx_MISO_GPIO_CLK | SPIx_MOSI_GPIO_CLK, ENABLE);
-
-//   //traba al programa (salta hrd_exception?)
-//   GPIO_PinAFConfig(SPIx_SCK_GPIO_PORT, SPIx_SCK_SOURCE, SPIx_SCK_AF);
-//   GPIO_PinAFConfig(SPIx_MOSI_GPIO_PORT, SPIx_MOSI_SOURCE, SPIx_MOSI_AF);
-//   GPIO_PinAFConfig(SPIx_MISO_GPIO_PORT, SPIx_MISO_SOURCE, SPIx_MISO_AF);
-
-//   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-//   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-//   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
-//   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_3;
-
-//   /* SPI SCK pin configuration */
-//   GPIO_InitStructure.GPIO_Pin = SPIx_SCK_PIN;
-//   GPIO_Init(SPIx_SCK_GPIO_PORT, &GPIO_InitStructure);
-
-//   //ademas lo pongo en 1 PB3
-//   //GPIOB->BSRR = 0x00000008;
-
-//   /* SPI  MOSI pin configuration */
-//   GPIO_InitStructure.GPIO_Pin =  SPIx_MOSI_PIN;
-//   GPIO_Init(SPIx_MOSI_GPIO_PORT, &GPIO_InitStructure);
-
-//   /* SPI MISO pin configuration */
-//   GPIO_InitStructure.GPIO_Pin = SPIx_MISO_PIN;
-//   GPIO_Init(SPIx_MISO_GPIO_PORT, &GPIO_InitStructure);
-
-//   /* SPI configuration -------------------------------------------------------*/
-
-//   //SPI_I2S_DeInit(SPIx);
-
-//   SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-//   SPI_InitStructure.SPI_DataSize = SPI_DATASIZE;
-//   //para 4021
-//   SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
-//   SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
-//   //para 74hc595 ok pero no el punto
-//   //SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
-//   //SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
-//   SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-//   //SPI_InitStructure.SPI_NSS = SPI_NSS_Hard;
-//   //SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
-//   //SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
-//   SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_8;	//a esta velocidad no responden los 4021
-//   SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
-// //  SPI_InitStructure.SPI_CRCPolynomial = 7;
-//   SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
-
-//   /* Configure the SPI interrupt priority */
-//   //NVIC_InitStructure.NVIC_IRQChannel = SPIx_IRQn;
-//   //NVIC_InitStructure.NVIC_IRQChannelPriority = 1;
-//   //NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-//   //NVIC_Init(&NVIC_InitStructure);
+/* Globals --------------------------------------------------------------------*/
 
 
-
-//   /* Initializes the SPI communication */
-//   SPI_Init(SPIx, &SPI_InitStructure);
-//   /* Enable the SPI peripheral */
-//   SPI_Cmd(SPIx, ENABLE);
-
-
-
-
-//   //Configuracion SPI
-//   SPIx->CR1 &= ~SPI_CR1_SPE;		//deshabilito periferico
-//   //clk / 256; master; CPOL High; CPHA second clock
-//   //SPIx->CR1 = 0;	//TODO: CR1 es el que tiene un error respecto de la config de arriba (verificar que puede ser) AHORRO MUCHA MEMORIA
-//   SPIx->CR2 = 0;
-//   //SPIx->CR1 |= SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_BR_2 | SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSM;
-//   SPIx->CR1 |= SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSM;
-//   //thresh 8 bits; data 8 bits;
-//   SPIx->CR2 |= SPI_CR2_FRXTH | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0;
-
-// //  SPIx->I2SCFGR &= (uint16_t)~((uint16_t)SPI_I2SCFGR_I2SMOD);
-//   SPIx->CR1 |= SPI_CR1_SPE;		//habilito periferico
-
-// }
-
+/* Module Functions -----------------------------------------------------------*/
 void SPI_Config(void)
 {
     unsigned long temp;
@@ -133,50 +36,17 @@ void SPI_Config(void)
         RCC_SPI1_CLK_ON;
 
     //Configuracion SPI
-    SPIx->CR1 = 0;
-    //SPI speed; clk / 256; master
-    // SPIx->CR1 |= SPI_CR1_BR_0 | SPI_CR1_BR_1 | SPI_CR1_BR_2;
+    SPI1->CR1 = 0;
+    //SPI speed; clk / 8; master
+    SPI1->CR1 |= SPI_CR1_BR_1;
     //CPOL High; CPHA second clock
-    SPIx->CR1 |= SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSM;
+    SPI1->CR1 |= SPI_CR1_MSTR | SPI_CR1_CPOL | SPI_CR1_CPHA | SPI_CR1_SSM | SPI_CR1_SSI;
 
     //thresh 8 bits; data 8 bits;
-    SPIx->CR2 = SPI_CR2_FRXTH | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0;
+    SPI1->CR2 = SPI_CR2_FRXTH | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0;
 
-    SPIx->CR1 |= SPI_CR1_SPE;		//habilito periferico
+    SPI1->CR1 |= SPI_CR1_SPE;		//habilito periferico
 }
-
-// void Send_SPI (unsigned char * p, unsigned char bytes)
-// {
-// 	/* Waiting until TX FIFO is empty */
-//     while (SPI_GetTransmissionFIFOStatus(SPIx) != SPI_TransmissionFIFOStatus_Empty);
-
-// 	pspi_tx = p;
-// 	spi_bytes_left = bytes;
-
-//     SPI_I2S_ITConfig(SPIx, SPI_I2S_IT_TXE, ENABLE);
-// }
-
-// unsigned char Send_Receive_SPI (unsigned char a)
-// {
-// 	unsigned char rx;
-// 	unsigned char j;
-
-// 	//primero limpio buffer rx spi
-// 	while ((SPIx->SR & SPI_SR_RXNE) == 1)
-// 	{
-// 		rx = SPI_ReceiveData8(SPIx);
-// 	}
-
-//     while (SPI_GetTransmissionFIFOStatus(SPIx) != SPI_TransmissionFIFOStatus_Empty);
-//     SPI_SendData8(SPIx, a);
-//     //espero tener el dato en RX
-//     for (j = 0; j < 150; j++)
-//     {
-//     	asm("nop");
-//     }
-
-//     return (SPI_ReceiveData8(SPIx));
-// }
 
 unsigned char Send_Receive_SPI (unsigned char a)
 {
@@ -184,15 +54,15 @@ unsigned char Send_Receive_SPI (unsigned char a)
 	unsigned char j;
 
 	//primero limpio buffer rx spi
-	while ((SPIx->SR & SPI_SR_RXNE) == 1)
+	while ((SPI1->SR & SPI_SR_RXNE) == 1)
 	{
-		rx = SPIx->DR & 0x0F;
+		rx = SPI1->DR & 0x0F;
 	}
 
 	//espero que haya lugar en el buffer
-	while ((SPIx->SR & SPI_TXE) == 0);
+	while ((SPI1->SR & SPI_TXE) == 0);
 
-	*(__IO uint8_t *) ((uint32_t)SPIx + (uint32_t)0x0C) = a; //evito enviar 16bits problemas de compilador
+	*(__IO uint8_t *) ((uint32_t)SPI1 + (uint32_t)0x0C) = a; //evito enviar 16bits problemas de compilador
 
     //espero tener el dato en RX
     for (j = 0; j < 150; j++)
@@ -200,99 +70,57 @@ unsigned char Send_Receive_SPI (unsigned char a)
     	asm("nop");
     }
 
-    return (SPIx->DR & 0x0F);
+    return (SPI1->DR & 0x0F);
 }
 
 void Wait_SPI_Busy (void)
 {
 	//espero que se transfiera el dato
-	while ((SPIx->SR & SPI_BSY) != 0);
+	while ((SPI1->SR & SPI_BSY) != 0);
 }
-// void Send_SPI_Multiple (unsigned char a)
-// {
-// 	//espero que haya lugar en el buffer
-// 	while ((SPIx->SR & SPI_TXE) == 0);
-
-// 	//*(__IO uint8_t *) SPIx->DR = a;
-// 	SPI_SendData8(SPIx, a);
-
-// }
 
 void Send_SPI_Multiple (unsigned char a)
 {
 	//espero que haya lugar en el buffer
-	while ((SPIx->SR & SPI_TXE) == 0);
+	while ((SPI1->SR & SPI_TXE) == 0);
 
 	//*(__IO uint8_t *) SPIx->DR = a;
-	*(__IO uint8_t *) ((uint32_t)SPIx + (uint32_t)0x0C) = a; //evito enviar 16bits problemas de compilador
+	*(__IO uint8_t *) ((uint32_t)SPI1 + (uint32_t)0x0C) = a; //evito enviar 16bits problemas de compilador
 
 }
-
-// void Send_SPI_Single (unsigned char a)
-// {
-// 	//espero que se libere el buffer
-// 	while ((SPIx->SR & SPI_TXE) == 0);
-
-// 	//tengo espacio
-// 	//SPIx->DR = a;
-// 	SPI_SendData8(SPIx, a);
-
-// 	//espero que se transfiera el dato
-// 	while ((SPIx->SR & SPI_BSY) != 0);
-// }
 
 void Send_SPI_Single (unsigned char a)
 {
 	//espero que se libere el buffer
-	while ((SPIx->SR & SPI_TXE) == 0);
+	while ((SPI1->SR & SPI_TXE) == 0);
 
 	//tengo espacio
 	//SPIx->DR = a;
 	//SPIx->DR = a;
-	*(__IO uint8_t *) ((uint32_t)SPIx + (uint32_t)0x0C) = a; //evito enviar 16bits problemas de compilador
+	*(__IO uint8_t *) ((uint32_t)SPI1 + (uint32_t)0x0C) = a; //evito enviar 16bits problemas de compilador
 
 	//espero que se transfiera el dato
-	while ((SPIx->SR & SPI_BSY) != 0);
+	while ((SPI1->SR & SPI_BSY) != 0);
 }
-
-// unsigned char Receive_SPI_Single (void)
-// {
-// 	unsigned char dummy;
-
-// 	//espero que se libere el buffer
-// 	while (((SPIx->SR & SPI_TXE) == 0) || ((SPIx->SR & SPI_BSY) != 0));
-
-// 	//limpio buffer RxFIFO
-// 	while ((SPIx->SR & SPI_RXNE) != 0)
-// 		dummy = SPIx->DR;
-
-// 	//SPIx->DR = 0xff;
-// 	SPI_SendData8(SPIx, 0xff);
-
-
-// 	//espero que se transfiera el dato
-// 	while ((SPIx->SR & SPI_BSY) != 0);
-
-// 	dummy = (unsigned char) SPIx->DR;
-// 	return dummy;
-// }
 
 unsigned char Receive_SPI_Single (void)
 {
 	unsigned char dummy;
 
 	//espero que se libere el buffer
-	while (((SPIx->SR & SPI_TXE) == 0) || ((SPIx->SR & SPI_BSY) != 0));
+	while (((SPI1->SR & SPI_TXE) == 0) || ((SPI1->SR & SPI_BSY) != 0));
 
 	//limpio buffer RxFIFO
-	while ((SPIx->SR & SPI_RXNE) != 0)
-		dummy = SPIx->DR;
+	while ((SPI1->SR & SPI_RXNE) != 0)
+		dummy = SPI1->DR;
 
-	*(__IO uint8_t *) ((uint32_t)SPIx + (uint32_t)0x0C) = 0xff; //evito enviar 16bits problemas de compilador
+	*(__IO uint8_t *) ((uint32_t)SPI1 + (uint32_t)0x0C) = 0xff; //evito enviar 16bits problemas de compilador
 
 	//espero que se transfiera el dato
-	while ((SPIx->SR & SPI_BSY) != 0);
+	while ((SPI1->SR & SPI_BSY) != 0);
 
-	dummy = (unsigned char) SPIx->DR;
+	dummy = (unsigned char) SPI1->DR;
 	return dummy;
 }
+
+//--- end of file ---//
--- src/spi.h
+++ src/spi.h
@@ -1,6 +1,20 @@
-#ifndef SPI_H
-#define SPI_H
+//---------------------------------------------
+// ##
+// ## @Author: Med
+// ## @Editor: Emacs - ggtags
+// ## @TAGS:   Global
+// ## @CPU:    STM32F030
+// ##
+// #### SPI.H #################################
+//---------------------------------------------
+#ifndef _SPI_H_
+#define _SPI_H_
 
+//----------- Defines For Configuration --------------//
+//----------- Some SPI Configurations ----------------//
+//----------- End of SPI Configurations --------------//
+
+//--- Exported constants ---//
 #define SPI_RXNE	0x0001
 #define SPI_TXE		0x0002
 #define SPI_BSY		0x0080
@@ -11,61 +25,8 @@
 #define RCC_SPI1_CLK_ON 		RCC->APB2ENR |= 0x00001000
 #define RCC_SPI1_CLK_OFF		RCC->APB2ENR &= ~0x00001000
 
-// desde que saque main.h
-/* Uncomment the line below if you will use the SPI peripheral as a Master */
-#define SPI_MASTER
-/* Uncomment the line below if you will use the SPI peripheral as a Slave */
-//#define SPI_SLAVE
 
-/* Uncomment the size of data to be transmetted (only one data size must be selected) */
-#define SPI_DATASIZE_8
-//#define SPI_DATASIZE_7
-//#define SPI_DATASIZE_6
-//#define SPI_DATASIZE_5
-
-/* Communication boards SPIx Interface */
-// #define SPIx                             SPI1
-#define SPIx_CLK                         RCC_APB2Periph_SPI1
-#define SPIx_IRQn                        SPI1_IRQn
-#define SPIx_IRQHandler                  SPI1_IRQHandler
-
-#define SPIx_SCK_PIN                     GPIO_Pin_3
-#define SPIx_SCK_GPIO_PORT               GPIOB
-#define SPIx_SCK_GPIO_CLK                RCC_AHBPeriph_GPIOB
-#define SPIx_SCK_SOURCE                  GPIO_PinSource3
-#define SPIx_SCK_AF                      GPIO_AF_0
-
-#define SPIx_MISO_PIN                    GPIO_Pin_4
-#define SPIx_MISO_GPIO_PORT              GPIOB
-#define SPIx_MISO_GPIO_CLK               RCC_AHBPeriph_GPIOB
-#define SPIx_MISO_SOURCE                 GPIO_PinSource4
-#define SPIx_MISO_AF                     GPIO_AF_0
-
-#define SPIx_MOSI_PIN                    GPIO_Pin_5
-#define SPIx_MOSI_GPIO_PORT              GPIOB
-#define SPIx_MOSI_GPIO_CLK               RCC_AHBPeriph_GPIOB
-#define SPIx_MOSI_SOURCE                 GPIO_PinSource5
-#define SPIx_MOSI_AF                     GPIO_AF_0
-
-/* Define data size and data masks */
-#ifdef SPI_DATASIZE_8
- #define SPI_DATASIZE                     SPI_DataSize_8b
- #define SPI_DATAMASK                     (uint8_t)0xFF
-#elif defined (SPI_DATASIZE_7)
- #define SPI_DATASIZE                     SPI_DataSize_7b
- #define SPI_DATAMASK                     (uint8_t)0x7F
-#elif defined (SPI_DATASIZE_6)
- #define SPI_DATASIZE                     SPI_DataSize_6b
- #define SPI_DATAMASK                     (uint8_t)0x3F
-#elif defined (SPI_DATASIZE_5)
- #define SPI_DATASIZE                     SPI_DataSize_5b
- #define SPI_DATAMASK                     (uint8_t)0x1F
-#else
- #error " Select at least one data size in spi.h file "
-#endif
-// fin desde que saque main.h
-
-//-------- Functions -------------
+//--- Exported Module Functions ------------
 void SPI_Config(void);
 void Send_SPI (unsigned char * , unsigned char);
 unsigned char Send_Receive_SPI (unsigned char a);
@@ -74,4 +35,4 @@ void Send_SPI_Multiple (unsigned char);
 void Send_SPI_Single (unsigned char);
 unsigned char Receive_SPI_Single (void);
 
-#endif
+#endif /* _SPI_H_ */
